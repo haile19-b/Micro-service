f626a3da301603ef7ed64e2bae47494896d63cb6
HaileGebriel <hailegebrielbekalu47@gmail.com>
2026-06-03 11:59:21 +0300

    feat(product-service): mount product routes and update server port log

--- services/product-service/src/server.ts
+++ services/product-service/src/server.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from 'express';
+import route from './products/product.routes';
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 
+app.use("/products",route)
+
 app.get('/health', (req: Request, res: Response) => {
   res.json({ status: 'UP', service: 'product-service' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Auth Service is running on port ${PORT}`);
+  console.log(`Product Service is running on port ${PORT}`);
 });
