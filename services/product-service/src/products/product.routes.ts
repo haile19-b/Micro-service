260470b8d037e37882983633f541294629451ce3
HaileGebriel <hailegebrielbekalu47@gmail.com>
2026-06-11 17:30:38 +0300

    feat(product-service): define product schemas and router endpoints

--- services/product-service/src/products/product.routes.ts
+++ services/product-service/src/products/product.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { ProductController } from "./product.controller.js";
 
 const route = Router();
+
+route.get("/", ProductController.getAllProducts);
+route.get("/:id", ProductController.getProductById);
+route.post("/", ProductController.createProduct);
 
 export default route;
