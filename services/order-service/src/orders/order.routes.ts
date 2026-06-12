7f5d49077c96b77c03ab877ee9cc892ed74758d3
HaileGebriel <hailegebrielbekalu47@gmail.com>
2026-06-12 21:47:10 +0300

    feat(order-service): define order routes

---
+++ services/order-service/src/orders/order.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { OrderController } from "./order.controller.js";
+
+const route = Router();
+
+route.post("/", OrderController.createOrder);
+route.get("/", OrderController.getOrdersByUser);
+
+export default route;
